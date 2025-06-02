4e3d1dfbe200737b13f2a1eedfaddf11f4214c52
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-06-02 21:44:46 +0300

    Enhance `projectSchema` with stricter validations, `timestamps`, and custom `versionKey`.

--- server/src/schemas/project.ts
+++ server/src/schemas/project.ts
@@ -1,10 +1,13 @@
 import { model, Schema } from "mongoose";
 
-const projectSchema = new Schema({
-  title: { type: String, required: true, unique: true },
-  content: { type: String, required: true },
-  link: { type: String },
-  repo: String,
-});
+const projectSchema = new Schema(
+  {
+    title: { type: String, required: true, unique: true, trim: true },
+    content: { type: String, required: true },
+    link: { type: String, trim: true },
+    repo: { type: String, unique: true, trim: true },
+  },
+  { timestamps: true, versionKey: "version" },
+);
 
 export const Project = model("Project", projectSchema);

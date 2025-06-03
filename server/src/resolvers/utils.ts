7f7ecaa2a2201ba696e895eda32a3500859f8655
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-06-03 18:48:09 +0300

    Refactor `mapDocument` utility for recursive ObjectId transformation
    - Enhanced `mapDocument` to support deep transformation of nested `_id` fields into `id` strings.
    - Added type safety with `TransformObjectId<T>` and improved handling of arrays, nested objects, and primitive types.

--- server/src/resolvers/utils.ts
+++ server/src/resolvers/utils.ts
@@ -1,9 +1,50 @@
 import { Types } from "mongoose";
 
-export const mapDocument = <T extends { _id: Types.ObjectId }>(doc: T) => {
-  const { _id, ...rest } = doc;
-  return {
-    id: _id.toString(),
-    ...rest,
-  };
+type ObjectId = Types.ObjectId;
+
+const isObjectId = (value: unknown): value is ObjectId =>
+  value instanceof Types.ObjectId;
+
+type TransformObjectId<T> = T extends ObjectId
+  ? string
+  : T extends Array<infer U>
+    ? Array<TransformObjectId<U>>
+    : T extends object
+      ? T extends {
+          _id: ObjectId;
+        }
+        ? Omit<{ [K in keyof T]: TransformObjectId<T[K]> }, "_id"> & {
+            id: string;
+          }
+        : { [K in keyof T]: TransformObjectId<T[K]> }
+      : T;
+
+export const mapDocument = <T>(doc: T): TransformObjectId<T> => {
+  if (doc === null || typeof doc !== "object") {
+    return doc as TransformObjectId<T>;
+  }
+
+  if (isObjectId(doc)) {
+    return doc.toString() as TransformObjectId<T>;
+  }
+
+  if (Array.isArray(doc)) {
+    return doc.map(mapDocument) as TransformObjectId<T>;
+  }
+
+  const result: Record<string, unknown> = {};
+
+  for (const key in doc) {
+    if (Object.prototype.hasOwnProperty.call(doc, key)) {
+      const value = doc[key];
+
+      if (key === "_id" && isObjectId(value)) {
+        result.id = value.toString();
+      } else {
+        result[key] = mapDocument(value);
+      }
+    }
+  }
+
+  return result as TransformObjectId<T>;
 };

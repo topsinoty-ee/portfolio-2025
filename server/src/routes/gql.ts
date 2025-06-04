ecbeaa15e6528263fdc092e6054a316d225954cc
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-06-05 02:15:24 +0300

    cleaned up commments

--- server/src/routes/gql.ts
+++ server/src/routes/gql.ts
@@ -2,13 +2,6 @@ import { FastifyPluginAsync } from "fastify";
 
 export const graphqlRoutes: FastifyPluginAsync = async (app) => {
   app.get("/", async (request, reply) => {
-    // const query = `{
-    //  sayHello(name: "Promise", message: "Hello World"){
-    //     name,
-    //     message
-    //   }
-    //  }`;
-    // return reply.graphql(query);
     return reply.redirect("/graphiql");
   });
 };
